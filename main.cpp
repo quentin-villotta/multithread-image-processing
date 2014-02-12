979646f526872a474e4da148cef2feff19b1de8d
alexandreyc <alexandre.crayssac@gmail.com>
2014-02-12 17:58:05 +0100

    Debug rgb to grayscale example

--- main.cpp
+++ main.cpp
@@ -9,23 +9,25 @@ int main(int argc, char *argv[])
 		cout << "Usage: ./a.out input.bmp output.bmp\n";
 		return 1;
 	}
-	//std::cout << "sizeof(Bitmap24FileHeader) = " << sizeof(Bitmap24FileHeader) << " bytes\n";
-	//std::cout << "sizeof(Bitmap24InfoHeader) = " << sizeof(Bitmap24InfoHeader) << " bytes\n";
 
 	Bitmap24 in(argv[1]);
 	Bitmap24 out(in.height, in.width);
 
+	double filter[] = {0.2989, 0.5870, 0.1140};
+
 	// RGB to grayscale
 	for(unsigned int i = 0; i < in.size; i++) {
-		uint8_t r = (uint8_t)in.pixels[3*i+2];
-		uint8_t g = (uint8_t)in.pixels[3*i+1];
-		uint8_t b = (uint8_t)in.pixels[3*i];
+		uint8_t r = in.pixels[3*i+2];
+		uint8_t g = in.pixels[3*i+1];
+		uint8_t b = in.pixels[3*i];
 
-		out.pixels[3*i+2] = (uint8_t)(0.2989 * r);
-		out.pixels[3*i+1] = (uint8_t)(0.5870 * g);
-		out.pixels[3*i] = (uint8_t)(0.1140 * b);
+		double tmp = filter[0]*r + filter[1]*g + filter[2]*b;
+
+		out.pixels[3*i+2] = (uint8_t)tmp;
+		out.pixels[3*i+1] = (uint8_t)tmp;
+		out.pixels[3*i] = (uint8_t)tmp;
 	}
-
+	
 	out.write(argv[2]);
 
 	return 0;
