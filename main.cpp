bbbd7fad4aa420f8a299aac8f06a93e586518310
alexandreyc <alexandre.crayssac@gmail.com>
2014-02-14 17:07:58 +0100

    Monothread linear filtering is now working

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 // Proof of concept for LinearFilter
 #include <iostream>
 #include <cstdint>
+#include <cstdio>
+#include <algorithm>
 
 #include "Bitmap24.h"
 #include "LinearFilter.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +17,8 @@ int main(int argc, char *argv[])
 	}
 
 	Bitmap24 in(argv[1]);
+	//in.print_headers();
+	// TODO : régler le bug d'output pour nature.bmp
 	Bitmap24 out(in.height, in.width);
 
 	// const int SIZE_NEIGHBORS = 1;
@@ -24,22 +30,55 @@ int main(int argc, char *argv[])
 	    -1,  8, -1,
 	    -1, -1, -1
 	};
+	// double t[] =
+	// {
+	//     0.0, 0.0, 0.0,
+	//     0.0, 1.0, 0.0,
+	//     0.0, 0.0, 0.0
+	// };
+	// double t[] =
+	// {
+	//      1,  1,  1,
+	//      1, -7,  1,
+	//      1,  1,  1
+	// };
+	// double t[] =
+	// {
+	//     1.0/9.0, 0, 0, 0, 0, 0, 0, 0, 0,
+	//     0, 1.0/9.0, 0, 0, 0, 0, 0, 0, 0,
+	//     0, 0, 1.0/9.0, 0, 0, 0, 0, 0, 0,
+	//     0, 0, 0, 1.0/9.0, 0, 0, 0, 0, 0,
+	//     0, 0, 0, 0, 1.0/9.0, 0, 0, 0, 0,
+	//     0, 0, 0, 0, 0, 1.0/9.0, 0, 0, 0,
+	//     0, 0, 0, 0, 0, 0, 1.0/9.0, 0, 0,
+	//     0, 0, 0, 0, 0, 0, 0, 1.0/9.0, 0,
+	//     0, 0, 0, 0, 0, 0, 0, 0, 1.0/9.0,
+	// };
+	// double t[] =
+	// {
+	//      0,  0,  0,  0,  0,
+	//      0,  0,  0,  0,  0,
+	//     -1, -1,  2,  0,  0,
+	//      0,  0,  0,  0,  0,
+	//      0,  0,  0,  0,  0,
+	// };
 	LinearFilter filter(3, 3, t);
+
+	int w = filter.width / 2;
+	int h = filter.height / 2;
 
 	// Apply filter
 	for(uint32_t pixel = 1; pixel <= in.size; pixel++) {
 		
 		int i_pixel = pixel / in.width + 1;
-		int j_pixel = pixel % in.width;
-		double r_sum, g_sum, b_sum = 0.0;
+		int j_pixel = pixel % in.width + 1;
+		double r_sum = 0.0, g_sum = 0.0, b_sum = 0.0;
 
 		// filter.width and filter.height must be odd
-		int w = filter.width / 2;
-		int h = filter.height / 2;
 		for(int i_neigh = -h; i_neigh <= h; i_neigh++) {
-			for(int j_neigh = -w; j_neigh <= h; j_neigh++) {
-				int i_neigh_img = i_pixel - i_neigh * in.width;
-				int j_neigh_img = j_pixel - j_neigh * in.height;
+			for(int j_neigh = -w; j_neigh <= w; j_neigh++) {
+				int i_neigh_img = i_pixel + i_neigh;
+				int j_neigh_img = j_pixel + j_neigh;
 
 				if(i_neigh_img <= 0 || i_neigh_img > in.height || j_neigh_img <= 0 || j_neigh_img > in.width)
 					continue;
@@ -49,16 +88,17 @@ int main(int argc, char *argv[])
 				uint8_t g_neigh = in.pixels[3*(pos_neigh_img-1)+1];
 				uint8_t b_neigh = in.pixels[3*(pos_neigh_img-1)];
 
-				int pos_neigh_filter = (i_neigh + h) * filter.width + j_neigh + h;
-				r_sum += filter.weights[pos_neigh_filter] * r_neigh;
-				g_sum += filter.weights[pos_neigh_filter] * g_neigh;
-				b_sum += filter.weights[pos_neigh_filter] * b_neigh;
+				int pos_neigh_filter = filter.width * (i_neigh + h) + j_neigh + w + 1;
+
+				r_sum += filter.weights[pos_neigh_filter-1] * r_neigh;
+				g_sum += filter.weights[pos_neigh_filter-1] * g_neigh;
+				b_sum += filter.weights[pos_neigh_filter-1] * b_neigh;
 			}
 		}
 
-		out.pixels[3*(pixel-1)+2] = (uint8_t) r_sum;
-		out.pixels[3*(pixel-1)+1] = (uint8_t) g_sum;
-		out.pixels[3*(pixel-1)] = (uint8_t) b_sum;
+		out.pixels[3*(pixel-1)+2] = (uint8_t) min(max(r_sum, 0.0), 255.0); 
+		out.pixels[3*(pixel-1)+1] = (uint8_t) min(max(g_sum, 0.0), 255.0); 
+		out.pixels[3*(pixel-1)] = (uint8_t) min(max(b_sum, 0.0), 255.0); 
 	}
 	
 	out.write(argv[2]);
