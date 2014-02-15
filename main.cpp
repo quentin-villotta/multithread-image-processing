2248def44697f2a7c4fb718d0b280e7ec0201598
alexandreyc <alexandre.crayssac@gmail.com>
2014-02-15 17:53:05 +0100

    Refactored for multithreading

--- main.cpp
+++ main.cpp
@@ -9,6 +9,52 @@
 
 using namespace std;
 
+void apply(const Bitmap24& in, Bitmap24& out, const LinearFilter& filter, uint32_t pixel)
+{
+	int w = filter.width / 2;
+	int h = filter.height / 2;
+
+	int i_pixel = pixel / in.width + 1;
+	int j_pixel = pixel % in.width + 1;
+	double r_sum = 0.0, g_sum = 0.0, b_sum = 0.0;
+
+	// filter.width and filter.height must be odd
+	for(int i_neigh = -h; i_neigh <= h; i_neigh++) {
+		for(int j_neigh = -w; j_neigh <= w; j_neigh++) {
+
+			int i_neigh_img = i_pixel + i_neigh;
+			int j_neigh_img = j_pixel + j_neigh;
+
+			if(i_neigh_img <= 0 || i_neigh_img > in.height || j_neigh_img <= 0 || j_neigh_img > in.width)
+				continue;
+
+			int pos_neigh_img = (i_neigh_img - 1) * in.width + j_neigh_img;
+			uint8_t r_neigh = in.pixels[3*(pos_neigh_img-1)+2];
+			uint8_t g_neigh = in.pixels[3*(pos_neigh_img-1)+1];
+			uint8_t b_neigh = in.pixels[3*(pos_neigh_img-1)];
+
+			int pos_neigh_filter = filter.width * (i_neigh + h) + j_neigh + w + 1;
+
+			//printf("r_neigh = %d\n", r_neigh);
+			r_sum += filter.weights[pos_neigh_filter-1] * r_neigh;
+			g_sum += filter.weights[pos_neigh_filter-1] * g_neigh;
+			b_sum += filter.weights[pos_neigh_filter-1] * b_neigh;
+		}
+	}
+	//printf("r_sum = %d\n", (uint8_t) min(max(r_sum, 0.0), 255.0));
+
+	out.pixels[3*(pixel-1)+2] = (uint8_t) min(max(r_sum, 0.0), 255.0); 
+	out.pixels[3*(pixel-1)+1] = (uint8_t) min(max(g_sum, 0.0), 255.0); 
+	out.pixels[3*(pixel-1)] = (uint8_t) min(max(b_sum, 0.0), 255.0);
+}
+
+void apply_range(const Bitmap24& in, Bitmap24& out, const LinearFilter& filter, uint32_t pixel_start, uint32_t pixel_end)
+{
+	for(uint32_t i = pixel_start; i < pixel_end; i++)
+		apply(in, out, filter, i);
+}
+
+
 int main(int argc, char *argv[])
 {
 	if(argc < 3) {
@@ -17,12 +63,7 @@ int main(int argc, char *argv[])
 	}
 
 	Bitmap24 in(argv[1]);
-	//in.print_headers();
-	// TODO : régler le bug d'output pour nature.bmp
 	Bitmap24 out(in.height, in.width);
-
-	// const int SIZE_NEIGHBORS = 1;
-	// double filter[3 * SIZE_NEIGHBORS] = ;
 
 	double t[] =
 	{
@@ -64,42 +105,7 @@ int main(int argc, char *argv[])
 	// };
 	LinearFilter filter(3, 3, t);
 
-	int w = filter.width / 2;
-	int h = filter.height / 2;
-
-	// Apply filter
-	for(uint32_t pixel = 1; pixel <= in.size; pixel++) {
-		
-		int i_pixel = pixel / in.width + 1;
-		int j_pixel = pixel % in.width + 1;
-		double r_sum = 0.0, g_sum = 0.0, b_sum = 0.0;
-
-		// filter.width and filter.height must be odd
-		for(int i_neigh = -h; i_neigh <= h; i_neigh++) {
-			for(int j_neigh = -w; j_neigh <= w; j_neigh++) {
-				int i_neigh_img = i_pixel + i_neigh;
-				int j_neigh_img = j_pixel + j_neigh;
-
-				if(i_neigh_img <= 0 || i_neigh_img > in.height || j_neigh_img <= 0 || j_neigh_img > in.width)
-					continue;
-
-				int pos_neigh_img = (i_neigh_img - 1) * in.width + j_neigh_img;
-				uint8_t r_neigh = in.pixels[3*(pos_neigh_img-1)+2];
-				uint8_t g_neigh = in.pixels[3*(pos_neigh_img-1)+1];
-				uint8_t b_neigh = in.pixels[3*(pos_neigh_img-1)];
-
-				int pos_neigh_filter = filter.width * (i_neigh + h) + j_neigh + w + 1;
-
-				r_sum += filter.weights[pos_neigh_filter-1] * r_neigh;
-				g_sum += filter.weights[pos_neigh_filter-1] * g_neigh;
-				b_sum += filter.weights[pos_neigh_filter-1] * b_neigh;
-			}
-		}
-
-		out.pixels[3*(pixel-1)+2] = (uint8_t) min(max(r_sum, 0.0), 255.0); 
-		out.pixels[3*(pixel-1)+1] = (uint8_t) min(max(g_sum, 0.0), 255.0); 
-		out.pixels[3*(pixel-1)] = (uint8_t) min(max(b_sum, 0.0), 255.0); 
-	}
+	apply_range(in, out, filter, 1, in.size + 1);
 	
 	out.write(argv[2]);
 
